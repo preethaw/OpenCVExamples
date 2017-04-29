a3a6dcf06845d8ce92991a987b9b170fe54cbf12
preethaw <preethawarrier@gmail.com>
2017-04-29 13:14:22 +0000

    Update opticdisk.py

--- opticdisk.py
+++ opticdisk.py
@@ -1,17 +1,31 @@
-import cv2
+# import the necessary packages
 import numpy as np
-from matplotlib import pyplot as plt
- 
-img = cv2.imread('images/01_dr.JPG',0)
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
- 
-#titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-titles = ['Original Image','Optic Disk',]
-images = [img, thresh1]
- 
-for i in xrange(2):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
- 
-plt.show()
+import argparse
+import cv2
+
+# load the image and convert it to grayscale
+image = cv2.imread("images/01_dr.JPG")
+
+orig = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+# the area of the image with the largest intensity value
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
+
+cv2.namedWindow("Naive", cv2.WINDOW_NORMAL) 
+# display the results of the naive attempt
+cv2.imshow("Naive", image)
+
+# apply a Gaussian blur to the image then find the brightest
+# region
+gray = cv2.GaussianBlur(gray, (141, 141), 0)
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+image = orig.copy()
+cv2.circle(image, maxLoc, 141, (255, 0, 0), 2)
+cv2.circle(image,maxLoc,1,(0,0,255),3)
+cv2.namedWindow("Robust", cv2.WINDOW_NORMAL) 
+# display the results of our newly improved method
+cv2.imshow("Robust", image)
+cv2.waitKey(0)
